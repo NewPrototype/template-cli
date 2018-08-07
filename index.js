c681684a8ae19312ac0400abf821d51bae31b371
liujunxiong <liujunxiong@quicloud.cn>
2018-08-07 15:53:31 +0800

    下载github项目

--- index.js
+++ index.js
@@ -3,11 +3,24 @@
 const program = require('commander');
 const download=require('download-git-repo');
 
+const ora = require('ora');
+
+
 const package = require('./package.json')
 
 program.version(package.version, '-v,--version').command('init <name>').action((name)=>{
   console.log('输入名称为:',name)
-  // download('')
+  const spinner = ora('正在下载模板...');
+  spinner.succeed();
+
+  download('github:NewPrototype/template-cli/#master',name,(err)=>{
+    console.log(err,'-----')
+    if(!err){
+      spinner.succeed();
+    }else {
+      spinner.fail();
+    }
+  })
 })
 
 program.parse(process.argv);
