bfaf74467c22882d5fa6038fcbe767cade6a7496
liujunxiong <15858209984@163.com>
2018-12-05 12:38:34 +0800

    增加ts模版命令

--- index.js
+++ index.js
@@ -12,14 +12,32 @@ var inquirer = require('inquirer');  //提示文本
 const package = require('./package.json'); //获取版本信息
 const re = new RegExp("^[a-zA-Z]+$"); //检查文件名是否是英文，只支持英文
 
+/**
+ * 不需要修改名称的文件
+ */
+const includes = ['index.ts', 'index.tsx', 'index.js', 'action.js', 'action.tsx', 'action.ts', 'reducer.js', 'reducer.ts', 'reducer.tsx', 'saga.ts', 'saga.js', 'saga.tsx', , 'README.md',]
+/**
+ * 模版对应分支
+ */
+const getType = {
+  "react-component------ES6组件": "component",
+  "react-function------函数组件": "master",
+  "react-redux------ES6组件": "redux",
+  "react-function------typescript 函数组件": "function-typescript",
+  "react-component------typescript ES6组件": "component-typescript",
+  "react-redux------typescript ES6组件": "redux-typescript",
+}
+
+
+
 program
   .version(package.version, '-v,--version')
   .command('init <name>')
   .action(name => {
     if (!re.test(name)) {
       console.log(symbols.error, chalk.red('错误!请输入英文名称'));
-      return 
-    } 
+      return
+    }
     if (!fs.existsSync(name)) {
       inquirer
         .prompt([
@@ -31,34 +49,35 @@ program
               'react-component------ES6组件',
               'react-function------函数组件',
               'react-redux------ES6组件',
-              'react-function-typescript------typescript 函数组件',
-              'react-component-typescript------typescript ES6组件',
+              'react-function------typescript 函数组件',
+              'react-component------typescript ES6组件',
+              'react-redux------typescript ES6组件',
             ],
           },
         ])
         .then(answers => {
-          console.log(symbols.success,chalk.green('开始创建..........,请稍候'));
+          console.log(symbols.success, chalk.green('开始创建..........,请稍候'));
           const spinner = ora('正在下载模板...');
           spinner.start();
-          const type = getType(answers)
+          const type = getType[answers.type]
           download(`github:NewPrototype/template/#${type}`, name, err => {
             if (err) {
               spinner.fail();
             } else {
               spinner.succeed();
               var files = fs.readdirSync(name);
-              for(let i=0;i<files.length;i++){
-                let fileName=`${name}/${files[i]}`;
-                if(fs.existsSync(`${name}/${files[i]}`)){
+              for (let i = 0; i < files.length; i++) {
+                let fileName = `${name}/${files[i]}`;
+                if (fs.existsSync(`${name}/${files[i]}`)) {
                   const content = fs.readFileSync(fileName).toString();
-                  const result = handlebars.compile(content)({template:name,});
+                  const result = handlebars.compile(content)({ template: name, });
                   fs.writeFileSync(fileName, result);
                 }
 
               }
               let count = 0; //所有文件修改完成，提示
               for (let i = 0; i < files.length; i++) {
-                if(files[i]=='index.ts'||files[i]=='index.js'||files[i]=='action.js'||files[i]=='reducer.js'||files[i]=='saga.js'||files[i]=='README.md'){
+                if (includes.includes(files[i])) {  //是否需要修改名称
                   continue
                 }
                 //获取文件列表
@@ -71,7 +90,7 @@ program
                       console.log('---错误');
                     }
                     count++;
-                    if (count+1 == files.length) { //排除index.js文件
+                    if (count + 1 == files.length) { //排除index.js文件
                       console.log(symbols.success, chalk.green('模版创建成功'));
                     }
                   }
@@ -87,28 +106,7 @@ program
 
 program.parse(process.argv);
 
-const getType = (type) => {
-  let str = 'master';
-  switch (type.type) {
-    case "react-component------ES6组件":
-      str = "component"
-      break;
-      case "react-function------函数组件":
-      str = "master"
-      break;
-      case "react-redux------ES6组件":
-      str = "redux"
-      break;
-      case "react-function-typescript------typescript 函数组件":
-      str = "function-typescript"
-      break;
-      case "react-component-typescript------typescript ES6组件":
-      str = "component-typescript"
-      break;
-    default:
-      break;
-  }
-  return str
-}
+
+
 
 
